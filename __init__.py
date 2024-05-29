7f1ab67fad6335da80cdbf8721dbc34e8fa76dec
miro <jarbasai@mailfence.com>
2024-05-29 22:28:35 +0100

    calculate when ISS is passing

--- __init__.py
+++ __init__.py
@@ -1,17 +1,20 @@
 import tempfile
-from datetime import datetime
-from os.path import join, dirname
+from datetime import datetime, timedelta
+from os.path import join
 from time import sleep
 
 import matplotlib.pyplot as plt
+import pytz
 import requests
 from lingua_franca.format import nice_duration
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from mpl_toolkits.basemap import Basemap
+from ovos_utils.time import to_local
 from ovos_workshop.decorators import intent_handler
 from ovos_workshop.decorators import resting_screen_handler
 from ovos_workshop.intents import IntentBuilder
 from ovos_workshop.skills import OVOSSkill
+from skyfield.api import Topos, load
 
 
 class ISSLocationSkill(OVOSSkill):
@@ -157,22 +160,17 @@ class ISSLocationSkill(OVOSSkill):
         sleep(1)
         self.gui.release()
 
-    # TODO - api has been deprecated, find replacement
-    #@intent_handler('when_iss.intent')
+    @intent_handler('when_iss.intent')
     def handle_when(self, message):
         lat = self.location["coordinate"]["latitude"]
         lon = self.location["coordinate"]["longitude"]
-        params = {"lat": lat, "lon": lon}
-        passing = requests.get("http://api.open-notify.org/iss-pass.json",
-                               params=params).json()
 
-        next_passage = passing["response"][0]
-        ts = next_passage["risetime"]
-        dt = datetime.fromtimestamp(ts)
-        delta = datetime.now() - dt
+        pred = SatellitePredictions(lat, lon, altitude=0, days=1).predict()
+        dt = pred["rise"]["time"]  # in user timezone
+        delta = pred["length"]
+
         duration = nice_duration(delta, lang=self.lang)
-        caption = self.location_pretty + " " + dt.strftime(
-            "%m/%d/%Y, %H:%M:%S")
+        caption = self.location_pretty + " " + dt.strftime("%m/%d/%Y, %H:%M:%S")
         image = self.generate_map(lat, lon)
 
         self.gui.show_image(image, caption=caption, fill='PreserveAspectFit')
@@ -222,13 +220,125 @@ class ISSLocationSkill(OVOSSkill):
         self.gui.release()
 
 
+class SatellitePredictions:
+    # taken from https://github.com/yuvadm/iss.guru/blob/master/iss/predictions.py
+    ISS = "ISS (ZARYA)"
+    STATIONS_URL = "http://celestrak.com/NORAD/elements/stations.txt"
+
+    def __init__(self, lat, lon, altitude=0, tz="UTC", satellite=ISS, start=None, days=10):
+        self.lat = lat
+        self.lon = lon
+        self.altitude = altitude
+        self.tz = tz
+        self.start = start
+        self.days = days
+
+        satellites = load.tle_file(self.STATIONS_URL)
+        self.satellite = {sat.name: sat for sat in satellites}[satellite]
+        self.location = Topos(latitude_degrees=self.lat, longitude_degrees=self.lon)
+
+    @staticmethod
+    def to_local_time(utc_iso: str):
+        """ensure datetime object is in user timezone"""
+        naive_datetime = datetime.strptime(utc_iso, '%Y-%m-%dT%H:%M:%SZ')
+        utc_timezone = pytz.timezone('UTC')
+        dt = utc_timezone.localize(naive_datetime)
+        return to_local(dt)
+
+    @staticmethod
+    def chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i: i + n]
+
+    @staticmethod
+    def deg_to_cardinal(deg):
+        cardinals = [
+            "N",
+            "NNE",
+            "NE",
+            "ENE",
+            "E",
+            "ESE",
+            "SE",
+            "SSE",
+            "S",
+            "SSW",
+            "SW",
+            "WSW",
+            "W",
+            "WNW",
+            "NW",
+            "NNW",
+        ]
+        return cardinals[round((deg % 360) / 22.5) % 16]
+
+    @staticmethod
+    def seconds_to_minutes(secs):
+        return f"{secs // 60}:{secs % 60:02}"
+
+    def get_next_days(self):
+        ts = load.timescale()
+        t0 = ts.now() if not self.start else ts.ut1_jd(self.start)
+        t1 = ts.ut1_jd(t0.ut1 + self.days)
+        return t0, t1
+
+    def get_position_details(self, t):
+        difference = self.satellite - self.location
+        topocentric = difference.at(t)
+        alt, az, distance = topocentric.altaz()
+        azimuth = int(az.degrees)
+        return {
+            "time": self.to_local_time(t.utc_iso()),
+            "degrees": int(alt.degrees),
+            "azimuth": azimuth,
+            "direction": self.deg_to_cardinal(azimuth),
+            "distance": int(distance.km),
+        }
+
+    def get_prediction_events(self):
+        t0, t1 = self.get_next_days()
+
+        ts, _events = self.satellite.find_events(
+            self.location, t0, t1, altitude_degrees=self.altitude
+        )
+
+        # events are returned as 3-tuples of (rise, culminate, set)
+        # where rise/set are relative to given altitude
+        # docs mention the possibility of several culminations
+        # https://rhodesmill.org/skyfield/earth-satellites.html#finding-when-a-satellite-rises-and-sets
+        # but this doesn't seem to happen in our case
+        res = list(self.chunks(ts, 3))
+
+        if len(res[-1]) != 3:
+            # truncate the last event in case it's a partial one
+            res = res[:-1]
+
+        return res
+
+    def predict(self):
+        preds = self.get_prediction_events()
+        rise, culminate, zet = preds[0]
+        length = int((zet - rise) * 86400)
+        return {
+            "length": timedelta(seconds=length),
+            "length_mins": self.seconds_to_minutes(length),
+            "rise": self.get_position_details(rise),
+            "culminate": self.get_position_details(culminate),
+            "set": self.get_position_details(zet),
+        }
+
+
 if __name__ == "__main__":
     from ovos_utils.fakebus import FakeBus
     from ovos_bus_client.message import Message
+    from ovos_config.locale import setup_locale
+    setup_locale()
+
 
     # print speak for debugging
     def spk(utt, *args, **kwargs):
         print(utt)
+
 
     s = ISSLocationSkill(skill_id="fake.test", bus=FakeBus())
     # s.update_picture()
@@ -240,5 +350,7 @@ if __name__ == "__main__":
     # Jasmin Moghbeli, Andreas Mogensen, Satoshi Furukawa, Konstantin Borisov, Oleg Kononenko, Nikolai Chub, Loral O'Hara are in orbit on board of the space station
     s.handle_iss(Message(""))
     # The international space station is now over Central African Republic at 8.6270 latitude 21.5912 longitude
+    s.handle_when(Message(""))
+    # The I S S will be over XXX in seven minutes twenty five seconds
     s.handle_about_iss_intent(Message(""))
     # The International Space Station is a modular space station in low Earth orbit. The ISS programme is a multi-national collaborative project between five participating space agencies: NASA ( United States ) , Roscosmos ( Russia ) , JAXA ( Japan ) , ESA ( Europe ) , and CSA ( Canada ) .The ownership and use of the space station is established by intergovernmental treaties and agreements.
